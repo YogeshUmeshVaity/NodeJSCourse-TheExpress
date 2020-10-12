76b01ad25252c77d12f059ade89a1f40708f9e12
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2020-10-13 02:21:59 +0530

    Handle '/' to display a welcome message.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express, { NextFunction, Request, Response } from 'express'
 
 const app = express()
+app.get('/', (request: Request, response: Response) => {
+  response.send('<h1>Welcome to Express!<h1>')
+})
 
 app.get(
   '/user/:id',
