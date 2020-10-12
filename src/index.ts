451ac96264a55d7529bfe670df3cc05ea88049a5
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2020-10-13 02:23:07 +0530

    Handle '/users' to display a list of users.

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,10 @@ app.get('/', (request: Request, response: Response) => {
   response.send('<h1>Welcome to Express!<h1>')
 })
 
+app.get('/users', (request: Request, response: Response) => {
+  response.send('<ul><li>Sandy</li><li>Jane</li><li>Pawn</li></ul>')
+})
+
 app.get(
   '/user/:id',
   (request: Request, response: Response, next: NextFunction) => {
